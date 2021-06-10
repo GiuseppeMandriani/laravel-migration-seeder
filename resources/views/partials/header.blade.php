22344d41839e2608cb2fd4d6846131a3c9774bac
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-10 20:54:40 +0200

    start css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,13 @@
-<header>
-   <h1>HEADER HERE</h1> 
-   <ul>
-      <li><a href="{{ route('home')}}">HOME</a></li>
-      <li><a href="{{ route('contacts')}}">Contact Us</a></li>
-   </ul>
+<header class="header">
+   <div class="container">
+      <a href="{{ route('home')}}">
+         SMARTBOX
+      </a>
+   </div>
+   <nav>
+      <ul>
+         <li><a href="{{ route('home')}}">HOME</a></li>
+         <li><a href="{{ route('contacts')}}">Contact Us</a></li>
+      </ul>
+   </nav>
 </header>
