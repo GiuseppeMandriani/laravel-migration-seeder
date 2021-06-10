4170f95aaa9fafe76e559a000dfe04ca3c05b895
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-10 19:39:33 +0200

    create row for table

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Package;
+
 class PackagesTableSeeder extends Seeder
 {
     /**
@@ -11,6 +13,26 @@ class PackagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 30; $i++){
+            // Creazione istanza
+            $newPackage = new Package();
+
+            // Popolazione colonne 
+
+            $newPackage->city = 'Foggia';
+            $newPackage->state = 'Italia';
+            $newPackage->n_person = rand(1,10);
+            $newPackage->period = rand(2,21);
+            $newPackage->type = 'Relax';
+            $newPackage->description = 'Lorem ipsum relax soggiorno per le persone che vuoi quando vuoi perchè a Foggia mangi tanto.';
+            $newPackage->price = rand(90, 5000) ;
+
+            
+
+
+            // Salva records
+
+            $newPackage->save();
+        }
     }
 }
